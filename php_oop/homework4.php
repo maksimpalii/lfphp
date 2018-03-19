c2469539672dca352ca02241778c0445f5b6a5dd
Maksim <maksim.palii@gmail.com>
2018-03-19 14:27:15 +0200

    homework#4

--- php_oop/homework4.php
+++ php_oop/homework4.php
@@ -1,69 +1,153 @@
 <?php
 
-trait TransDirectionBack
+trait MoveDirection
 {
-    function _TransDirectionBack()
+    public function MoveDirection()
     {
-        echo 'TransDirectionBack';
+        switch ($this->GetDirection()) {
+            case 'назад':
+                echo 'Включаете передачу назад ' . PHP_EOL;
+                break;
+            case 'вперед':
+                echo 'Включаете передачу вперед ' . PHP_EOL;
+                break;
+        }
     }
 }
 
 trait mechanism
 {
-    use TransDirectionBack;
+    use MoveDirection;
 
-    function Engine($speed, $distance)
+    abstract public function Engine();
+
+    public function GetHorsePower()
     {
-        //echo ' dvishok rabotaet ' . $speed;
+        echo 'Двигатель: ' . $this->GetSpeed() / 2 . ' лошадиных сил' . PHP_EOL;
     }
 
-    function EngineStart()
+    public function TransmissionAuto()
     {
-        echo 'вы включаете двигатель' . PHP_EOL;
+        $this->MoveDirection();
     }
 
-    function Cooler()
+    public function TransmissionManual()
     {
-
-    }
-
-    function TransmissionAuto($direction)
-    {
-        if ($direction === 'вперед') {
-            $this->EngineStart();
+        $this->MoveDirection();
+        $speed = $this->GetSpeed();
+        if ($speed > 0 && $speed <= 20) {
+            echo 'Включаем 1-ю передачу' . PHP_EOL;
+        } else {
+            echo 'Включаем 2-ю передачу' . PHP_EOL;
         }
     }
+}
 
-    function TransmissionManual()
-    {
-        echo 'transManual';
-    }
+interface EngineWork
+{
+    public function EngineStart();
+
+    public function EngineStop();
+
+    public function EngineWork();
+}
+
+abstract class Car
+{
+    public $name;
+
+    abstract public function __construct();
+
+    abstract public function moveCar($distance, $speed, $direction);
+
+    abstract public function GetSpeed();
+
+    abstract public function GetDistance();
+
+    abstract public function GetDirection();
 
 }
 
-
-class Car
+class Niva extends Car implements EngineWork
 {
-    public $name;
+    private $speed;
+    private $direction;
+    private $distance;
+    protected $temperatur;
     use mechanism;
 
-    public function __construct($name)
+    public function __construct($name = 'Niva')
     {
         $this->name = $name;
     }
 
     public function moveCar($distance, $speed, $direction)
     {
-        $this->TransmissionAuto($direction);
-        // echo $this->name . $distance . $speed . $direction;
-        //$this->Engine($speed);
-        // $this->_TransDirectionBack();
+        $this->distance = $distance;
+        $this->speed = $speed;
+        $this->direction = $direction;
+        $this->GetHorsePower();
+        if ($speed >= 1) {
+            $this->Engine();
+        }
     }
 
+    public function GetSpeed()
+    {
+        return $this->speed;
+    }
+
+    public function GetDistance()
+    {
+        return $this->distance;
+    }
+
+    public function GetDirection()
+    {
+        return $this->direction;
+    }
+
+    public function Engine()
+    {
+        $this->EngineStart();
+        $this->TransmissionAuto();
+        //$this->TransmissionManual();
+        $this->EngineWork();
+
+    }
+
+    public function EngineStart()
+    {
+        echo 'Вы включаете двигатель' . PHP_EOL;
+    }
+
+    public function EngineStop()
+    {
+        echo 'Выключаете двигатель' . PHP_EOL;
+    }
+
+    public function EngineWork()
+    {
+        $distance = $this->distance;
+        for ($distanceNull = 0, $this->temperatur = 0; $distanceNull <= $distance; $distanceNull += 10, $this->temperatur += 5) {
+            echo 'Машина проехала:  ' . $distanceNull . PHP_EOL;
+            echo $this->Cooler($this->temperatur) . PHP_EOL;
+            if ($distanceNull === $distance) {
+                $this->EngineStop();
+            }
+        }
+    }
+
+    private function Cooler()
+    {
+        echo 'Температура двигателя :' . $this->temperatur . PHP_EOL;
+        if ($this->temperatur == 90) {
+            echo 'Включить куллер' . PHP_EOL;
+            $this->temperatur = 80;
+        }
+    }
 }
 
-
-$Niva = new Car('Niva');
-$Niva->moveCar(200, 10, 'вперед');
-//$Niva->workEngine();
-//$Niva->TransmissionAuto();
+$Niva = new Niva();
+echo $Niva->name . PHP_EOL;
+$Niva->moveCar(250, 10, 'вперед');
